e1ab4c09f47b4690231747e93a7b51e31db7604b
Omar Alaa <omar.skillful@gmail.com>
2019-08-13 18:05:51 +0200

    Replacing the global var jQuery with dollar sign

--- public/js/index.js
+++ public/js/index.js
@@ -11,33 +11,32 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log(message);
-    let li = jQuery('<li></li>');
+    let li = $('<li></li>');
     li.text(`${message.from}: ${message.text}`);
 
-    jQuery('#messages').append(li);
+    $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
-    let li = jQuery('<li></li>');
-    let a = jQuery('<a target="_blank">My current location</a>')
+    let li = $('<li></li>');
+    let a = $('<a target="_blank">My current location</a>')
     li.text(`${message.from}: `);
     a.attr('href', message.url);
     li.append(a);
-    jQuery('#messages').append(li);
+    $('#messages').append(li);
 });
 
-jQuery('#message-form').on('submit', function(e) {
+$('#message-form').on('submit', function(e) {
     e.preventDefault();
-
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: $('[name=message]').val()
     }, function() {
 
     });
 });
 
-let locationButton = jQuery('#send-location');
+let locationButton = $('#send-location');
 locationButton.on('click', function() {
     if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
